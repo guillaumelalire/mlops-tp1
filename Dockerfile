f5442a9b77e421e3d97539964ca16dbf1681f5c5
Guillaume Lalire <guillaume.lalire@epita.fr>
2024-10-23 16:46:49 +0200

    feat: added model for song recommendation

--- Dockerfile
+++ Dockerfile
@@ -5,7 +5,9 @@ WORKDIR /code
 COPY ./requirements.txt /code/requirements.txt
 RUN pip install --no-cache-dir --upgrade -r /code/requirements.txt
 
-COPY ./regression.joblib /code/regression.joblib
+COPY ./models/regression.joblib /code/models/regression.joblib
+COPY ./models/music_knn.joblib /code/models/music_knn.joblib
+COPY ./data/song_list.csv /code/data/song_list.csv
 COPY ./app.py /code/app.py
 
 CMD ["fastapi", "run", "app.py", "--port", "80"]
--- app.py
+++ app.py
@@ -1,10 +1,23 @@
 import joblib
+import pandas as pd
 from fastapi import FastAPI
 
 app = FastAPI()
 
 @app.get("/predict")
 async def root(size: int, nb_bedrooms: int, garden: int):
-    model = joblib.load('regression.joblib')
-    print(size, nb_bedrooms, garden)
+    model = joblib.load('models/regression.joblib')
     return model.predict([[size, nb_bedrooms, garden]])[0]
+
+@app.get("/predict_song")
+async def song_prediction(song_name: str):
+    song_list = pd.read_csv('data/song_list.csv')
+    song_index = song_list[song_list['track_name'].str.lower() == song_name.lower()].index
+    song_features = song_list[['track_popularity', 'track_album_release_date', 'energy', 'key', 'loudness', 'speechiness', 'acousticness', 'instrumentalness', 'liveness', 'valence', 'tempo', 'duration_ms']]
+
+    model = joblib.load('models/music_knn.joblib')
+    distances, neighbours = model.kneighbors(song_features.loc[song_index], 11, return_distance=True)
+    
+    music_recommendations = song_list.iloc[neighbours[0]][['track_name', 'track_artist']]
+    
+    return music_recommendations[1:].T
